dad47f5c78b19a9b08118c0e5af52cb51098b92c
Andrew Kairys <andrewkairys@gmail.com>
2018-10-11 14:21:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ function sayHey() {
 }
 
 function sayHeyFriend(name) {
-  return `Hey ${name}!`;
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(string) {
+  return string.capitalize;
 }
